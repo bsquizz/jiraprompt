56c4c639fd80c9e4d8bb7479451f1448904d0379
Brandon Squizzato <bsquizza@redhat.com>
2018-02-19 17:50:59 -0500

    Setup/install scripts

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='simplejira',
+    version='1.0',
+    description='simplejira',
+    author='Brandon Squizzato',
+    author_email='bsquizza@redhat.com',
+    url='https://www.github.com/bsquizz/simplejira',
+    packages=['simplejira'],
+    install_requires=[
+        'jira',
+        'pyyaml',
+        'prompter',
+        'python-editor',
+        'attrs',
+        'prettytable',
+        'cmd2',
+        'iso8601',
+        'six',
+        'pykerberos',
+        'python-dateutil',
+        'requests',
+    ],
+    scripts=['bin/simplejira']
+)
